93598eeb60d54962dcca1aaa26145fcccebb305e
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-20 15:31:00 +0100

    add: update request restaurant

---
+++ app/Http/Requests/UpdateRestaurantRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRestaurantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3|max:120|unique:restaurants,name',
+            'email' => 'required|email|unique:restaurants,email',
+            'phone' => 'required|max:15',
+            'address' => 'required',
+            'p_iva' => 'required|min:11|max:11|unique:restaurants,p_iva',
+            'categories' => ['exists:categories,id', 'required']
+        ];
+    }
+}
