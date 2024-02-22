ea47c42e63cf7f797995dc30059aa6c010b32683
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-02-22 16:41:16 +0100

    fix: orders seeder

--- resources/views/partials/go_back.blade.php
+++ resources/views/partials/go_back.blade.php
@@ -1,1 +1,2 @@
-<a class="btn btn-warning mt-2" style="color:white " href="{{url()->previous()}}"><- Go Back</a>
+<a class="btn btn-warning mt-2" style="color:white " href="{{url()->previous()}}"><i
+  class="fa-solid fa-arrow-rotate-left"></i> Go Back</a>
