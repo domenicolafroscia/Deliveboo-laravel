b5d5c155a69c6a89de05149820e8ad3f6106d525
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-26 22:23:03 +0100

    add: function makePayment in order controller

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -12,12 +12,36 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function generateToken(Request $request, Gateway $gateway) {
+    public function generateToken(Request $request, Gateway $gateway)
+    {
         $token = $gateway->clientToken()->generate();
         return response()->json([
             'success' => true,
             'token' => $token
         ]);
+    }
+
+    public function makePayment(Request $request, Gateway $gateway)
+    {
+        $results = $gateway->transaction()->sale([
+            'amount' => $request->amount, //spesa totale fare controlli con id prodotti
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if($results->success) {
+            return response()->json([
+                'results' => true,
+                'message' => 'Payment success'
+            ]);
+        } else {
+            return response()->json([
+                'results' => false,
+                'message' => 'Payment denied'
+            ]);
+        }
     }
 
     // public function store(StoreOrderRequest $request) {
@@ -42,11 +66,11 @@ class OrderController extends Controller
 
     //     $order = new Order();
     //     $order->fill($form_data);
-        
-        
+
+
     //     // $order->price_tot = $price_order_tot;
     //     $order->save();
-                        
+
     //     // foreach($form_data->cart as $item) {
     //     //     $order->meals()->attach($item->meals_id, ['quantity' => $item->quantity]);
     //     // } 
