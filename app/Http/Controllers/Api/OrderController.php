2e902e07ac03a81aa731813677343791fc7bcbf3
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-02-26 22:14:01 +0100

    add: generate token function

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -7,44 +7,53 @@ use App\Http\Requests\StoreOrderRequest;
 use App\Models\Meal;
 use App\Models\Order;
 use App\Models\Restaurant;
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function store(StoreOrderRequest $request) {
-        $form_data = $request->validated();
-
-                    // check id meals for price and restaurant
-                    //   $collection = collect($response->json())
-                    // $meals_id = $collection->cart->pluck('meal_id');
-                    // $meals = Meal::whereIn('id', $meals_id)->get();
-                    // $price_order_tot = 0;
-                    // foreach($meals as $meal) {
-                    //     $price_order_tot += $meal->price;
-                    // }
-
-                    // $restaurant = Restaurant::whereIn('id', $meals_id)->get();
-                    // if(count($restaurant > 1)) {
-                    //     return response()->json([
-                    //         'success' => false,
-                    //         'results' => 'Two different Restaurants'
-                    //     ]);
-                    // }
-
-        $order = new Order();
-        $order->fill($form_data);
-        
-        
-        // $order->price_tot = $price_order_tot;
-        $order->save();
-                        
-        // foreach($form_data->cart as $item) {
-        //     $order->meals()->attach($item->meals_id, ['quantity' => $item->quantity]);
-        // } 
-
+    public function generateToken(Request $request, Gateway $gateway) {
+        $token = $gateway->clientToken()->generate();
         return response()->json([
             'success' => true,
-            'results' => $form_data
+            'token' => $token
         ]);
     }
+
+    // public function store(StoreOrderRequest $request) {
+    //     $form_data = $request->validated();
+
+    //                 // check id meals for price and restaurant
+    //                 //   $collection = collect($response->json())
+    //                 // $meals_id = $collection->cart->pluck('meal_id');
+    //                 // $meals = Meal::whereIn('id', $meals_id)->get();
+    //                 // $price_order_tot = 0;
+    //                 // foreach($meals as $meal) {
+    //                 //     $price_order_tot += $meal->price;
+    //                 // }
+
+    //                 // $restaurant = Restaurant::whereIn('id', $meals_id)->get();
+    //                 // if(count($restaurant > 1)) {
+    //                 //     return response()->json([
+    //                 //         'success' => false,
+    //                 //         'results' => 'Two different Restaurants'
+    //                 //     ]);
+    //                 // }
+
+    //     $order = new Order();
+    //     $order->fill($form_data);
+        
+        
+    //     // $order->price_tot = $price_order_tot;
+    //     $order->save();
+                        
+    //     // foreach($form_data->cart as $item) {
+    //     //     $order->meals()->attach($item->meals_id, ['quantity' => $item->quantity]);
+    //     // } 
+
+    //     return response()->json([
+    //         'success' => true,
+    //         'results' => $form_data
+    //     ]);
+    // }
 }
