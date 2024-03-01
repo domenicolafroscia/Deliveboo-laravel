19be8d692bc472506739db56383e41f06d505238
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-03-01 18:05:48 +0100

    refactoring: not found page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,19 +4,20 @@
     <div class="container">
 
         <div class="d-flex align-items-center justify-content-center vh-100">
-            <div class="text-center row">
+            <div class="justify-content-center row box-not-found">
 
-                <div class=" col-md-6">
+                
+                <div class="not-found col-md-6 d-none d-lg-block"></div>
+                {{-- <div class=" col-md-6 d-lg-none">
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNRbF2c1k9SnwBoDcwhVREGcK9xWuSUCamRQ&usqp=CAU" alt=""
                         class="img-fluid" style="height: 250px">
-                </div>
-                
+                </div> --}}
                 <div class=" col-md-6 mt-5">
                     <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
                     <p class="lead">
                         The page you're looking for doesn't exist.
                     </p>
-                    <a class="btn btn-primary" href="{{ route('admin.restaurants.index') }}">Go Home</a>
+                    <a class="btn btn-primary mb-3" href="{{ route('admin.restaurants.index') }}">Go Home</a>
                 </div>
     
             </div>
