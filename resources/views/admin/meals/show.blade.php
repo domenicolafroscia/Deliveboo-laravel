db5d160d7568fe315a8851a4dc7ee3e41812924b
gianmarcocilia <gianmarcocilia97@gmail.com>
2024-03-04 10:48:39 +0100

    add: style for show meal

--- resources/views/admin/meals/show.blade.php
+++ resources/views/admin/meals/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
+    {{-- <div class="container">
         @include('partials.go_back')
         <h2 class="py-3 text-center">{{ $meal->name }}</h2>
     </div>
@@ -20,5 +20,37 @@
         <h5 class="text-danger">Meal not available</h5>
     @endif
 
-    <p>{{$meal->description}}</p>
+    <p>{{$meal->description}}</p> --}}
+    <h2 class="text-center py-3">Preview for {{$meal->name}}</h2>
+    <div class="row justify-content-center">
+
+        <div class="col-10">
+            <div class="dish-card">
+                @if ($meal->image)
+                    <img class="dish-image"
+                        src="{{ str_contains($meal->image, 'https') ? $meal->image : asset('storage/' . $meal->image) }}"
+                        alt="{{ $meal->name }}" style="height: 450px">
+                @else
+                    <h5 class="text-center">Image not found</h5>
+                @endif
+                <div class="dish-details text-center">
+                    <h3 class="dish-title">{{ $meal->name }}</h3>
+                    <p class="dish-description">{{ $meal->description }}</p>
+                    <p class="dish-price">{{ $meal->price . '€' }}</p>
+                    <div class="actions">
+                        {{-- <a class="btn btn-success" href="{{ route('admin.meals.show', ['meal' => $meal->slug]) }}">
+                <i class="fa-solid fa-info"></i>
+            </a> --}}
+                        <a class="btn btn-green" href="{{ route('admin.meals.edit', ['meal' => $meal->slug]) }}">
+                            <i class="fa-solid fa-pencil"></i> Edit
+                        </a>
+                        <a class="btn btn-green" href="{{ route('admin.restaurants.index') }}">
+                            <i class="fa-solid fa-check"></i> Save
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
